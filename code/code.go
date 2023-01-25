04a481797368e15f603ca7a23f2d749fec351cdb
Marc-Antoine <magiguere@mirego.com>
2023-01-25 11:38:29 -0500

    implement depends-on field on profile

--- code/code.go
+++ code/code.go
@@ -10,9 +10,15 @@ import (
 func InstallExtensions(profile config.Profile) {
 	println("installing extensions for profile '" + profile.Name + "'")
 	cmd_args := []string{".", "--extensions-dir", profile.Path}
-	for _, ext := range profile.Extensions {
-		cmd_args = append(cmd_args, "--install-extension", ext)
+	cmd_args = append(cmd_args, getInstallArgs(profile.Extensions)...)
+
+	if len(profile.DependsOn) > 0 {
+		for i, dep_exts := range profile.DependsOn {
+			fmt.Printf("installing dependency %d/%d for profile '%s'\n", i+1, len(profile.DependsOn), profile.Name)
+			cmd_args = append(cmd_args, getInstallArgs(dep_exts)...)
+		}
 	}
+
 	cmd_args = append(cmd_args, "--force")
 
 	runCmd(cmd_args)
@@ -23,6 +29,14 @@ func LaunchCode(profile config.Profile) {
 	cmd_args := []string{".", "--extensions-dir", profile.Path}
 
 	runCmd(cmd_args)
+}
+
+func getInstallArgs(exts []string) []string {
+	var cmd_args = []string{}
+	for _, ext := range exts {
+		cmd_args = append(cmd_args, "--install-extension", ext)
+	}
+	return cmd_args
 }
 
 func runCmd(cmd_args []string) {
