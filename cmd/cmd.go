80a940095a83999a11076c6848cc55fe6c99ce4b
Marc-Antoine <magiguere@mirego.com>
2023-01-29 13:58:37 -0500

    add `install` command

---
+++ cmd/cmd.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"code-profiles/utils"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	verboseFlag bool
+	installFlag bool
+)
+
+// Execute runs the main cobra command for cli `code-profiles`.
+func Execute() {
+	var rootCmd = &cobra.Command{Use: "code-profiles"}
+
+	openCmd.Flags().BoolVarP(&installFlag, "install", "i", false, "should install extensions before opening vscode")
+	openCmd.Flags().BoolVarP(&verboseFlag, "verbose", "v", false, "prints additional logs")
+
+	installCmd.Flags().BoolVarP(&verboseFlag, "verbose", "v", false, "prints additional logs")
+
+	rootCmd.AddCommand(openCmd)
+	rootCmd.AddCommand(installCmd)
+
+	utils.Check(rootCmd.Execute())
+}
