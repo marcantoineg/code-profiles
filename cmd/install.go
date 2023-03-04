d3b3c6852a66544c6b42335ac4a06eac72342df0
Marc-Antoine Giguère <magiguere@mirego.com>
2023-03-04 14:58:06 -0500

    replace `~` with home directory (#21)

--- cmd/install.go
+++ cmd/install.go
@@ -5,6 +5,7 @@ import (
 	"code-profiles/config"
 	"code-profiles/utils"
 
+	"github.com/marcantoineg/fileutil"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +13,7 @@ var installCmd = &cobra.Command{
 	Use:   "install [profile_name]",
 	Short: "install required VSCode extensions for a given profile",
 	Run: func(cmd *cobra.Command, args []string) {
-		config.SetConfigPath(configPathFlag)
+		config.SetConfigPath(fileutil.ReplaceTilde(configPathFlag))
 
 		var profileName = ""
 		if len(args) >= 1 {
